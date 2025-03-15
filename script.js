0c5ae5d3d115f89ee05b6073010e094c901a89c2
brian <bayebrian89@gmail.com>
2025-03-15 09:58:28 +0100

    my complete project with tailwind

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-let list= document.getElementById('list')
-   list.style.maxHeight = '0px';
 
-   function toggleMenu(){
-    if (list.style.maxHeight == '0px') {
-      list.style.minHeight = '300px';
-    }
-    else{
-      list.style.maxHeight = '0px';
-    }
+   list.style.maxHeight = '0px';
+   function Menu(e){
+    let list= document.querySelector('ul');
+    e.name ==='menu' ? (e.name="close",list.classList.
+      add('top-[80px]')): (e.name='menu',list.classList.
+      remove('top-[80px]'))
    }
