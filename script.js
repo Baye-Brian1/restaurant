83af00138adaf2d3e632bb1cfaecd11989f5844c
brian <bayebrian89@gmail.com>
2025-03-19 09:07:29 +0100

    responsive restaurant

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-
-   list.style.maxHeight = '0px';
-   function Menu(e){
-    let list= document.querySelector('ul');
-    e.name ==='menu' ? (e.name="close",list.classList.
-      add('top-[80px]')): (e.name='menu',list.classList.
-      remove('top-[80px]'))
+   
+   //  let list= document.querySelector('ul');
+   //  e.name ==='menu' ? (e.name="close",list.classList.
+   //    add('top-[80px]')): (e.name='menu',list.classList.
+   //    remove('top-[80px]'))
+   
+   function Menu(){
+   let list= document.querySelector('ul');
+   let menuIcon= document.getElementById('icon');
+    if 
+    (list.classList.contains('top-[80px]')) {
+      list.classList.remove('top-[80px]');
+      menuIcon.innerText= '=';
    }
+    else{
+      list.classList.add('top-[80px]');
+      menuIcon.innerText= 'x';
+    }
+   }
+  
